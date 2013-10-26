8ba5161a08e678bee88a6d2ef598506501977346
PhuongNQ <nquangphuong@gmail.com>
2013-10-26 17:47:29 +0900

    add Mailer module

---
+++ Demo/Classes/Mailer.h
@@ -0,0 +1,13 @@
+//
+//  Mailer.h
+//  SMTPSender
+//
+//  Created by PhuongNQ on 10/26/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mailer : NSObject
+
+@end
---
+++ Demo/Classes/Mailer.m
@@ -0,0 +1,13 @@
+//
+//  Mailer.m
+//  SMTPSender
+//
+//  Created by PhuongNQ on 10/26/13.
+//
+//
+
+#import "Mailer.h"
+
+@implementation Mailer
+
+@end
